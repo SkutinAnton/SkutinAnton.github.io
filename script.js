f373cabf6f29dcd70b1ce79469122a4079ddb2b3
SkutinAnton <Skutin_anton@mail.ru>
2018-02-04 22:04:37 +0300

    add JS

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-alert("HHH");
+function show() {
+    
+    function showLogo() {
+        let logo = document.querySelector('.logo_hide');
+        logo.classList.remove('logo_hide');
+    }
+    
+    function showName() {
+        let logo = document.querySelector('.header__name_hide');
+        logo.classList.remove('header__name_hide');
+    }
+
+    function showProfession() {
+        let logo = document.querySelector('.header__profession_hide');
+        logo.classList.remove('header__profession_hide');
+    }
+
+    setTimeout(showLogo, 300);
+    setTimeout(showName, 1400);
+    setTimeout(showProfession, 2000);
+}
+
+show();
